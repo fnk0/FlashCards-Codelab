2ec486947ed814ad4111aba864ad882a206e8e91
Marcus Gabilheri <marcusandreog@gmail.com>
2014-09-07 15:55:12 -0500

    changed icon, added assets

--- app/src/main/java/gabilheri/com/flashcards/MainActivity.java
+++ app/src/main/java/gabilheri/com/flashcards/MainActivity.java
@@ -1,42 +1,24 @@
 package gabilheri.com.flashcards;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.widget.DrawerLayout;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ListView;
+import android.widget.BaseAdapter;
 
 
-public class MainActivity extends Activity {
-
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
+public class MainActivity extends DrawerLayoutActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.drawer_activity);
+    public void init() {
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void displayView(int position, Bundle fragmentBundle) {
+
     }
 
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected BaseAdapter getAdapter() {
+        return null;
     }
 }
