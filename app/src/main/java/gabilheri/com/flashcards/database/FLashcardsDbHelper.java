11b1faabedb9c07ec453ef65cd63ca9241f82703
Marcus Gabilheri <marcusandreog@gmail.com>
2014-09-20 06:04:48 -0500

    finishing create table queries

--- app/src/main/java/gabilheri/com/flashcards/database/FLashcardsDbHelper.java
+++ app/src/main/java/gabilheri/com/flashcards/database/FLashcardsDbHelper.java
@@ -36,13 +36,15 @@ public class FLashcardsDbHelper extends SQLiteOpenHelper {
      * DECKS DATABASE CONSTANTS
      */
     public static final String DECKS_TABLE = "decks";
-    public static final String DECKS_FLASHCARDS = "flashcards";
+    public static final String DECKS_CATEGORY = "category";
 
     /**
      * FLASHCARDS DATABASE TABLE
      */
     public static final String FLASHCARDS_TABLE = "flashcards";
     public static final String FLASHCARD_CONTENT = "content";
+    public static final String FLASHCARD_ANSWER = "answer";
+    public static final String FLASHCARD_BELONGS_TO = "belongs_to";
 
 
     public FLashcardsDbHelper(Context context) {
@@ -52,12 +54,45 @@ public class FLashcardsDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        final String CREATE_CATEGORY_TABLE = "";
+        final String SQL_CREATE_CATEGORY_TABLE = "CREATE TABLE " + CATEGORIES_TABLE + " (" +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                TITLE + " TEXT NOT NULL, " +
+                "FOREIGN KEY (" + CATEGORY_DECKS + ") REFERENCES " + DECKS_TABLE + " (" + _ID + ")" +
+                ";";
 
+        final String SQL_CREATE_DECKS_TABLE = "CREATE TABLE " + DECKS_TABLE + " (" +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                TITLE + " TEXT NOT NULL, " +
+                "FOREIGN KEY (" + DECKS_CATEGORY + ") REFERENCES + " + CATEGORIES_TABLE + " (" + _ID + ")" +
+                ";";
+
+        final String SQL_CREATE_FLASHCARDS_TABLE = "CREATE TABLE " + FLASHCARDS_TABLE +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                TITLE + " TEXT NOT NULL, " +
+                FLASHCARD_CONTENT + " TEXT NOT NULL, " +
+                FLASHCARD_ANSWER + " TEXT NOT NULL, " +
+                "FOREIGN KEY (" + FLASHCARD_BELONGS_TO + ") REFERENCES " + DECKS_TABLE + " (" + _ID + ")" +
+                ";";
+
+        // We now create our tables.
+        // If the tables already exist Android will them ignore this statement.
+        db.execSQL(SQL_CREATE_CATEGORY_TABLE);
+        db.execSQL(SQL_CREATE_DECKS_TABLE);
+        db.execSQL(SQL_CREATE_FLASHCARDS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // For the purpose of this tutorial we will have the simplest method of upgrading a Database.
+        // When dealing within your own apps use ALTER TABLE instead...
+        // The reason of not using Alter Table right now is because it can be very specific for each case
 
+        // This statements will delete the current tables and call onCreate with the new create table queries.
+        // We will never be using this in this tutorial but keep in mind that onUpgrade will be called whenever you want
+        // to add a field to the Database.
+        db.execSQL("DROP TABLE IF EXISTS " + CATEGORIES_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + DECKS_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + FLASHCARDS_TABLE);
+        onCreate(db);
     }
 }
